4a8a9fab5a5cf3056c1b818948b44eba5a1a1240
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-18 15:46:55 -0400

    updated favicon; updated app name; created Header.js and imported into App.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="wrapper">
+				<h1>Pupper Play Date</h1>
+			</div>
+		)
+	}
+}
+
+export default Header;
