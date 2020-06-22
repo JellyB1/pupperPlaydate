dfd52238cf3e7256ae20788ac7906b9d5ef6e73c
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-22 09:56:24 -0400

    Updates

---
+++ precache-manifest.2205bff4aca10153b9b92dc7c27db76e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73a4226f8979ebaf07faa366c90c3fd",
+    "url": "/angelicaChungProjectFive/index.html"
+  },
+  {
+    "revision": "0a3c5fcfb2939b3a0f5b",
+    "url": "/angelicaChungProjectFive/static/css/main.15f971f7.chunk.css"
+  },
+  {
+    "revision": "b8f69f71b3f8d526040e",
+    "url": "/angelicaChungProjectFive/static/js/2.baf547ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/angelicaChungProjectFive/static/js/2.baf547ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3c5fcfb2939b3a0f5b",
+    "url": "/angelicaChungProjectFive/static/js/main.ddb40510.chunk.js"
+  },
+  {
+    "revision": "bc1876555ad017394fdf",
+    "url": "/angelicaChungProjectFive/static/js/runtime-main.bbf57866.js"
+  },
+  {
+    "revision": "52e2d5b657c013e594c6af47143d0b36",
+    "url": "/angelicaChungProjectFive/static/media/headerImg.52e2d5b6.jpg"
+  },
+  {
+    "revision": "55c8a2b4ce33a77ce3e1933f185ff01f",
+    "url": "/angelicaChungProjectFive/static/media/loadingGif.55c8a2b4.gif"
+  }
+]);
