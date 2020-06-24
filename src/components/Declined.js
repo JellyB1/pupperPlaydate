1274e5b487087b18d8de5f83427f0c5cda3272eb
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-24 13:23:22 -0400

    added refresh button to Declined.js

--- src/components/Declined.js
+++ src/components/Declined.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import img from '../assets/declined.gif';
 
+const refreshPage = () => {
+	window.location.reload();
+}
+
 const Declined = () => {
 	return (
 		<div className='wrapper'>
 			<div className='declined'>
 				<i class="fas fa-times-circle"></i>
 				<h3>Oh no! Your playdate has been declined!</h3>
+			
+				<div className='imgCD'>
+					<img src={img} alt='Sleepy pupper animated gif'/>
+				</div>
+			
+				<button type='submit' onClick={refreshPage}>Try again!</button>
 			</div>
-			<div className='imgCD'>
-				<img src={img} alt='Sleepy pupper animated gif'/>
-			</div>
+		
+		
 		</div>
 	)
 }
