d76f0910826f023ed11cdc9861a7097f9959b53f
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-24 09:55:08 -0400

    fixed confirmation modal

--- src/App.js
+++ src/App.js
@@ -25,29 +25,33 @@ class  App extends Component {
 		};	
 	}
 
-	componentDidMount() {
+	async componentDidMount() {
 		// Make Axios request
-		axios({
-			method: 'GET',
-			url: 'https://api.thedogapi.com/v1/images/search?apiKey=16ec4811-e569-402f-b549-dea6709fcca2&limit=20',
-			responseType: 'JSON',
-		}).then(res => {
-			// Store the result data so its easier to work with
-			const result = res.data;
-			
-			// map over res.data and return an array due to nesting issues in res.data
-			const myData = result.map(item => {
-				return ({
-					doggoURL: item.url,
-					doggoID: item.id
+		try {
+			await axios({
+				method: 'GET',
+				url: 'https://api.thedogapi.com/v1/images/search?apiKey=16ec4811-e569-402f-b549-dea6709fcca2&limit=20',
+				responseType: 'JSON',
+			}).then(res => {
+				// Store the result data so its easier to work with
+				const result = res.data;
+				
+				// map over res.data and return an array due to nesting issues in res.data
+				const myData = result.map(item => {
+					return ({
+						doggoURL: item.url,
+						doggoID: item.id
+					});
 				});
-			});
 
-			// push that returned array into this.state
-			this.setState({
-				doggo: myData,
-			});
-		}); // End axios request
+				// push that returned array into this.state
+				this.setState({
+					doggo: myData,
+				});
+			}); // End axios request
+		} catch {
+			alert("Oh no! Something doesn't seem right!")
+		}
 	}
 
 	scrollToDoggoSelection = () => {
